681f8f3ca8375969291320783565b28a47630805
anwachuku15 <anwachuku15@gmail.com>
2019-11-27 21:26:06 -0500

    user authentication

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,54 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
 admin.initializeApp();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD-wiJ0MiZc1yTVa-07Y2owWQTfQsXf8n0",
+  authDomain: "social-3be8a.firebaseapp.com",
+  databaseURL: "https://social-3be8a.firebaseio.com",
+  projectId: "social-3be8a",
+  storageBucket: "social-3be8a.appspot.com",
+  messagingSenderId: "90837447855",
+  appId: "1:90837447855:web:ad36dae6562b6fa3a8d05a",
+  measurementId: "G-2XEB4Q5QHP"
+};
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello Andrew!");
-});
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const db = admin.firestore();
 
 // GET DOCUMENTS
-exports.getPosts = functions.https.onRequest((req, res) => {
-	admin
-		.firestore()
-		.collection('posts')
+app.get('/posts', (req, res) => {
+	db.collection('posts')
+		.orderBy('createdAt', 'desc')
 		.get()
 		.then(data => {
 			let posts = []
 			data.forEach(doc => {
-				posts.push(doc.data());
+				posts.push({
+					postId: doc.id,
+					...doc.data()
+				});
 			});
 			return res.json(posts);
 		})
 		.catch(err => console.error(err));
-});
+})
 
 // CREATE DOCUMENTS
-exports.createPost = functions.https.onRequest((req, res) => {
-	if(req.method !== 'POST'){
-		return res.status(400).json({ error: 'Method not allowed'});
-	}
-	
+app.post('/post', (req, res) => {
+
 	const newPost = {
 		body: req.body.body,
 		userHandle: req.body.userHandle,
-		createdAt: admin.firestore.Timestamp.fromDate(new Date())
+		createdAt: new Date().toISOString()
 	};
 
-	admin
-		.firestore()
-		.collection('posts')
+	db.collection('posts')
 		.add(newPost)
 		.then(doc => {
 			res.json({ message: `document ${doc.id} created successfully!` });
@@ -47,4 +57,58 @@ exports.createPost = functions.https.onRequest((req, res) => {
 			res.status(500).json({ error: 'something went wrong'});
 			console.error(err);
 		})
+	
 });
+
+
+// SIGN-UP ROUTE
+app.post('/signup', (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword,
+		handle: req.body.handle,
+	};
+	// TODO: validate data
+	let token, userId
+	db.doc(`/users/${newUser.handle}`)
+		.get()
+		.then(doc => {
+			if(doc.exists) {
+				return res.status(400).json({ handle: 'this is handle is already taken' })
+			} else {
+				return (
+					firebase
+						.auth()
+						.createUserWithEmailAndPassword(newUser.email, newUser.password)
+				)
+			}
+		})
+		.then((data) => {
+			userId = data.user.uid;
+			return data.user.getIdToken();
+		})
+		.then((idToken => {
+			token = idToken;
+			const userCredentials = {
+				handle: newUser.handle,
+				email: newUser.email,
+				createdAt: new Date().toISOString(),
+				userId
+			};
+			return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+		}))
+		.then(() => {
+			return res.status(201).json({ token });
+		})
+		.catch(err => {
+			console.error(err);
+			if(err.code === 'auth/email-already-in-use'){
+				return res.status(400).json({ email: 'Email is already in use'})
+			} else {
+				return res.status(500).json({ error: err.code });
+			}
+		})
+})
+
+exports.api = functions.https.onRequest(app);
